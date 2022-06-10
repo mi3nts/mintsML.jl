1ec9464d281cda51ea95ceaa3c10af48b4cb3627
Documenter.jl <documenter@juliadocs.github.io>
2022-06-10 13:52:01 +0000

    build based on 77d3fdf

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = mintsML","category":"page"},{"location":"#mintsML","page":"Home","title":"mintsML","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for mintsML.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [mintsML]","category":"page"}]
+}
